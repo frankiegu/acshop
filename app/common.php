ac6237ca404d615997c15a9e81656ddcff28757a
小涛 <1073519986@qq.com>
2020-10-18 10:39:53 +0800

    push

--- app/common.php
+++ app/common.php
@@ -1,2 +1,60 @@
 <?php
+// +----------------------------------------------------------------------
+// | AcShop (Acgice商城)
+// +----------------------------------------------------------------------
+// | 团队官网: https://oauth.acgice.com
+// +----------------------------------------------------------------------
+// | 联系我们: https://oauth.acgice.com/sdk/contact.html
+// +----------------------------------------------------------------------
+// | gitee开源项目：https://gitee.com/orzice/acshop
+// +----------------------------------------------------------------------
+// | Author：Orzice(小涛)  https://gitee.com/orzice
+// +----------------------------------------------------------------------
+// | DateTime：2020-10-18 10:34:40
+// +----------------------------------------------------------------------
 // 应用公共文件
+
+if (!function_exists('http_query')) {
+function http_query($url, $post = null)
+   {
+        // 初始化一个cURL会话
+        $ch = curl_init($url);
+        if (isset($post)) {
+            curl_setopt($ch, CURLOPT_POST, TRUE);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        }
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);  
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5); 
+        //忽略证书
+        if (substr($url, 0, 5) == 'https') {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        }
+        $curl_result = curl_exec($ch);
+        if ($curl_result) {
+            $data = $curl_result;
+        } else {
+            $data = curl_error($ch);
+        }
+        curl_close($ch);    #关闭cURL会话
+        return $data;
+    }
+ }
+ 
+if (!function_exists('__url')) {
+
+    /**
+     * 构建URL地址
+     * @param string $url
+     * @param array $vars
+     * @param bool $suffix
+     * @param bool $domain
+     * @return string
+     */
+    function __url(string $url = '', array $vars = [], $suffix = true, $domain = false)
+    {
+        return url($url, $vars, $suffix, $domain)->build();
+    }
+}
